1b4e1ad40579a934d133d67016e0e541fb9de704
ev3rs0u1 <210504430@qq.com>
2022-03-26 17:54:45 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::net::{IpAddr, Ipv4Addr};
+use trust_dns_resolver::Resolver;
+use trust_dns_resolver::error::ResolveResult;
+use trust_dns_resolver::{config::*, lookup::TxtLookup};
+
 fn main() {
-    println!("Hello, world!");
+    let query = "o-o.myaddr.l.google.com.";
+    let config = ResolverConfig::from_parts(
+        None,
+        vec![],
+        NameServerConfigGroup::from_ips_clear(
+            &[
+                IpAddr::V4(Ipv4Addr::new(216, 239, 32, 10)),
+                IpAddr::V4(Ipv4Addr::new(216, 239, 34, 10)),
+            ],
+            53,
+            false),
+    );
+
+    let resolver = Resolver::new(config, ResolverOpts::default()).unwrap();
+    let response = resolver.txt_lookup(query);
+    display_txt(&response);
+}
+
+fn display_txt(response: &ResolveResult<TxtLookup>) {
+    match response {
+        Err(_) => println!("No Records."),
+        Ok(resp) => {
+            for record in resp.iter() {
+                println!("{}", record.to_string());
+            }
+        }
+    }
 }
